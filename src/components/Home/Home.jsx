6fa3cd952f96350d0f01b89faec92ee82caa3fa1
dagemawinegash <dagemawibekele04@gmail.com>
2024-12-01 14:58:40 +0300

    added popular cities page modified college list page and popular universities page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Hero/Hero'
 import Popular from '../Popular/Popular'
 import TrackCollegeList from '../TrackCollegeList/TrackCollegeList'
+import City from '../City/City'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <Popular/>
       <TrackCollegeList/>
+      <City/>
     </div>
   )
 }
