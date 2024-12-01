2b2202d00a7d6daaebc4d0daa21e216140353d7d
dagemawinegash <dagemawibekele04@gmail.com>
2024-12-01 13:06:09 +0300

    added track your college list page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
 import Popular from '../Popular/Popular'
+import TrackCollegeList from '../TrackCollegeList/TrackCollegeList'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <TrackCollegeList/>
     </div>
   )
 }
