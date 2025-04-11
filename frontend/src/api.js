8923b015598912fa172fc52064162f09181762f0
dagemawinegash <dagemawibekele04@gmail.com>
2025-04-12 00:45:55 +0300

    making changes on the backend for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const isDevelopment = import.meta.env.MODE === 'development';
+const myBaseUrl = isDevelopment
+  ? import.meta.env.VITE_API_BASE_URL_LOCAL
+  : import.meta.env.VITE_API_BASE_URL_DEPLOY;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: myBaseUrl,
   headers: {
     'X-Secret-Key': import.meta.env.VITE_SECRET_KEY,
   },
