d20a07fe73648885794af0f24692bff1a26198ca
dagemawinegash <dagemawibekele04@gmail.com>
2025-05-19 21:17:35 +0300

    testing database connection

---
+++ backend/api/tests/test_database_connection.py
@@ -0,0 +1,11 @@
+from django.db import connections
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+class DatabaseConnectionTest(TestCase):
+    def test_database_connection(self):
+        db_conn = connections['default']
+        try:
+            db_conn.cursor()
+        except OperationalError:
+            self.fail("Database connection failed")
